5171afded47a73eb557c10eb22b020ae8db2334f
Aryan Kumar Keshri <Aryankehsri00@gmail.com>
2025-12-28 17:33:55 +0530

    Aryan

---
+++ Data_type/script.js
@@ -0,0 +1,178 @@
+// document.write("Wlecome to JavaScript!");
+
+// console.log("Wlecome to JavaScript!");
+
+// var x = 30; Correct
+// var 121x = 30; incorrect
+// var _x = 30; correct
+// var $x = 30; Correct
+
+// var firstName = "Aryan"; 
+// var firstname = "Aryan";
+
+// Note :- Both are different type variable name.
+// Because Javascript is a case sensitive language.
+
+// let KeyWord :-
+
+// let x = 10;
+
+// if(x>5){
+//     let y = 20;
+//     console.log(y);
+// }
+// // console.log(y);
+// console.log("object");
+
+// Const KeyWord:-
+
+// const x = 10;
+
+// console.log(x);
+
+// x=20;
+
+// Scope :-
+
+// Global Scope :-
+
+// var x = "Aryan";
+// function eg(){
+//     console.log(x);
+// }
+// eg();
+// console.log(x);
+
+
+//Function Scope :-
+
+// function eg(){
+//     var fs = "Hello, Aryan"
+//     console.log(fs);
+// };
+// eg();
+// console.log(fs);
+
+//Block Scope :-
+
+// function Eg(){
+//     if(true){
+//         let bv = "Aryan";
+//         console.log(bv);
+//     }
+//     console.log(bv);
+// }
+// Eg();
+
+//Data Types:-
+
+// Primitive                          Reference
+
+// String                              Object
+// Number                              Array
+// Boolean                             Function
+// null
+// undefined
+// BigInt
+// Symbol
+
+//  Primitive Data Type:-
+
+// String DT:-
+
+// In Javascript, a string is sequence of zero or more characters.
+
+// A string starts and ends with a Either single quote (') or a double quote (").
+
+// Javascript string are for storing and manipulating text.
+
+// Example:-
+
+// let firstName = "Aryan";
+// let SecoundName = 'Keshri';
+
+// Number DT:-
+
+// Number represents integer and floating-point numbers.
+
+// let num = 20.3243;
+// console.log(num);
+
+// Boolean DT:-
+
+// The boolean data type has two values: True and False.
+
+// let x = true;
+// let y = false;
+
+// console.log(x , y);
+// console.log(typeof(x , y));
+
+// let num = 20 > 1.30;
+// console.log(num);
+
+// Undefined DT :-
+
+// If the variable is declared but the value is not assigned, then the value of that variable will be undefined.
+
+// And the data type is also undefined
+
+// let age = 20;
+
+// console.log(age);
+// console.log( typeof age);
+
+
+// Null DT:-
+
+// In Javascript, null is special value that represents empty or unknown value.
+
+// let num = null;
+// console.log(num);
+// console.log( typeof num);
+
+// Reference Date Type:-
+
+// Object:-
+
+// in JavaScript, An object is a collection of properties.
+
+// Where every property is defined as a key-value pair.
+
+// let preson = {
+//     firstName : "aryan",
+//     lastName : "keshri",
+//     age : 20
+// };
+// console.log(preson);
+// console.log(typeof preson);
+
+// Array DT:-
+
+// Array are a type of object that stores a collection of values.
+
+// let num = [1,3,4,5,6,7,2];
+// console.log(num);
+// console.log(typeof num);
+
+// function DT:-
+
+// Function are a type of object that can be used to execute code.
+
+// function msg(){
+//     console.log("HEllo, ARyan");
+// }
+// console.log(msg);
+// msg();
+
+// Data Types:-
+
+// Javascript is a dynamically typed language.
+
+// So we can store different data type in same variable.
+
+// let x;
+// x = true;
+// console.log(typeof x);
+// console.log(x);
+
---
+++ Operators/script.js
@@ -0,0 +1,141 @@
+// operators in JavaScript
+
+// Operators in Javascript are symbols
+
+// They are used to perform Operations on operands.
+
+// Operands are the value and variable.
+
+// 10 + 20;
+
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// String operators
+
+//Arithmetic operators :-
+
+// Arithmetic operators are used to perform mathematical Operations on operands.
+
+// Addition
+// let sum = 5 + 3;
+// console.log(sum); // 8
+
+// Subtraction
+// let difference = 10 - 4;
+// console.log(difference); // 6
+
+// Multiplication
+// let product = 2 * 6;
+// console.log(product); // 12
+
+// Division
+// let quotient = 15 / 3;
+// console.log(quotient); // 5
+
+// Remainder (Modulus)
+// let remainder = 17 % 4;
+// console.log(remainder); // 1
+
+// Exponentiation
+// let result = 2 ** 4;
+// console.log(result); // 16
+
+// Assignment Operator:-
+
+// Assignment operators are used to assign values to variables. We use (=) sign for assignment
+// operator.
+
+//  Example:
+
+// let x = 5;
+// We have multiple assignment operator like:
+// Addition assignment
+// x += 3;
+// console.log("x:", x); // x: 8
+// // Subtraction assignment
+// x -= 2;
+// console.log("x:", x); // x: 6
+// // Multiplication assignment
+// x *= 4;
+// console.log("x:", x); // x: 24
+// // Division assignment
+// x /= 3;
+// console.log("x:", x); // x: 8
+// // Remainder (Modulus) assignment
+// x %= 5;
+// console.log("x:", x); // x: 3
+// // Exponentiation assignment
+// x **= 2;
+// console.log("x:", x); // x: 9
+
+
+// Increment / Decrement:-
+
+// The increment and decrement operators are used to increase or decrease the value of a variable by 1. 
+
+// The increment operator is (++) and, the decrement operator is (--).
+
+// The increment and decrement operators can be used in two ways:
+
+// Prefix and Postfix. 
+
+// Let’s learn Prefix increment and decrement operators:
+
+// let a = 10;
+// console.log(++a);
+// console.log(a);
+
+// In this example operator is placed before the variable, and the value of the variable is incremented before it is used.
+
+// let a = 10;
+// console.log(--a);
+// console.log(a);
+
+// Let’s learn Postfix increment and decrement operators:
+
+// let a = 10;
+// console.log(a++);
+// console.log(a);
+
+// In this example operator is placed after the variable, and the value of the variable is used before it is incremented.
+
+// let a = 10;
+// console.log(a--);
+// console.log(a);
+
+
+// Comparison operators:-
+
+// Comparison operators compare two values and give back a boolean value: either true or false.
+
+//  Comparison operators are useful in decision-making and loop programs in JavaScript. 
+
+// Let’s see some examples:
+
+// < (less than)
+// > (greater than)
+// <= (less than or equal to)
+// >= (greater than or equal to) == (Equal checks )
+// != (inequality) (not equal) (flipped value of equal checks) ===(strict equality checks ) (checks the Data type)
+// !==(strict inequality (!==) (flipped value strict equality checks)
+
+
+// Comparison operators :-
+
+// const a = 10;
+// const b = 20;
+// console.log(a < b); // true
+// console.log(a > b); // false
+// console.log(a <= b); // true
+// console.log(a >= b); // false
+// console.log(a == b); // false
+// console.log(a != b); // true
+// console.log(a === b); // false
+// console.log(a !== b); // true
+
+// const a = "10";
+// const b = 10;
+// console.log(a == b); // true
+// console.log(a === b); // false
